10f740781a60401a25a44be7a1306453adc922d0
Phillip Freitas <phillipjonathan@gmail.com>
2017-01-03 15:49:05 -0200

    Edit and Delete actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,22 @@ class ItemsController < ApplicationController
   		render new
 	end
   end
+
+  def edit
+  end
+
+  def update
+  	if @item.update(item_params)
+  		redirect_to item_path(@item)
+  	else
+  		render edit
+	end
+  end
+
+  def destroy
+  	@item.destroy
+  	redirect_to root_path
+  end
   
   private
 
